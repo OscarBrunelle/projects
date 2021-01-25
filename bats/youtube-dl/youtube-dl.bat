162e7999fee1a5b7e638ab7fd12a961fe99565dc
Oscar Brunelle <oscar.brunelle@gmail.com>
2021-01-25 23:05:42 +0100

    shorter version of ffmpeg

--- bats/youtube-dl/youtube-dl.bat
+++ bats/youtube-dl/youtube-dl.bat
@@ -2,7 +2,7 @@
 set /p url=Enter the Youtube URL, or leave empty to exit: 
 :loop
 	if "%url%" == "" (exit)
-	youtube-dl.exe -o "%%USERPROFILE%%\Music\%%(title)s.%%(ext)s" -x --audio-format mp3 --add-metadata %url%
-	echo(
+	set url=%url:music.youtube=youtube%
+	youtube-dl.exe -o "%%USERPROFILE%%\Music\%%(title)s.%%(ext)s" -x --audio-format mp3 --add-metadata -i %url%
 	set /p url=Enter the Youtube URL, or leave empty to exit: 
 	goto loop
